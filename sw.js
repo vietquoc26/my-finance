adff541fb3160dcebc2c700ef2af6ba9a23c9a75
vietquoc26 <vietquoc150799@gmail.com>
2026-09-16 22:18:28 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    mất mạng, và để trình duyệt cho phép "Cài đặt ứng dụng" (yêu cầu bắt buộc
    của PWA). Tăng số ở CACHE mỗi lần bạn cập nhật app để buộc nạp lại cache. */
 var CACHE = "so-tai-chinh-v1";
-var ASSETS = ["./finance_app.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
+var ASSETS = ["./my_finance.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }).then(function(){ return self.skipWaiting(); }));
